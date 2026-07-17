6bb40a78060fe29ace046fac6c5f650798d89f6d
sumit <sumitsah250@gmail.com>
2026-07-17 17:29:02 +0545

    some minor chnages

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -6,17 +6,23 @@ from django.http import HttpResponse
 # request handler
 # action 
 
-def say_hello(request):
+def calculate ():
     x=12
+    y=13
+    z=x+y
+    return z
+
+def say_hello(request):
+    x=calculate()
     y=13
     return render(request, "hellow.html")
 
 
 
 
-def say_no(request):
-    number = 12
-    if(number>10):
-        return HttpResponse("Yes")
-    else:
-        return HttpResponse("No")
+# def say_no(request):
+#     number = 12
+#     if(number>10):
+#         return HttpResponse("Yes")
+#     else:
+#         return HttpResponse("No")
